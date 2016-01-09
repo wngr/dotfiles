3ae8070e62c86ebd705c97d6c778a304a4341c74
Oliver Wangler <oliver@wngr.de>
2016-01-09 17:31:48 +0100

    added mic/audio ctrl scripts for i3

---
+++ i3wm/i3MicToggle.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#bindsym XF86AudioMicMute exec ~/Projects/linux-utils/i3wm/i3MicToggle.sh
+
+function setState {
+for source in $(pacmd list-sources | grep -oE 'index: [0-9]+' | awk '{ print $2 }'); do
+   echo $source
+    pactl set-source-mute $source $1
+done
+}
+
+#get curr state
+amixer get Capture | grep '\[off\]'
+if [[ $? -eq 0 ]]; then
+    setState 0
+else
+    setState 1
+fi
