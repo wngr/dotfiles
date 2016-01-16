bcca7ff52b057c9d061be557ba3dba14dcafc1b5
Oliver Wangler <oliver@wngr.de>
2016-01-16 20:09:03 +0100

    added optional parameter througput to screenshotLock

--- i3wm/screenshotLock.sh
+++ i3wm/screenshotLock.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
+#runs one additional argument, e.g. -n for nofork
 scrot /tmp/screen_locked.png
 convert /tmp/screen_locked.png -scale 10% -scale 1000% /tmp/screen_locked2.png
-i3lock -i /tmp/screen_locked2.png
+i3lock $1 -i /tmp/screen_locked2.png
 rm /tmp/screen_locked.png
 rm /tmp/screen_locked2.png
