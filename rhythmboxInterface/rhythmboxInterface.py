b4c5d673e6b36fab2daef1d2a4b94e8734dd6a0b
Oliver Wangler <oliver@wngr.de>
2016-01-17 17:04:32 +0100

    added handling for cases, when rhythmbox is not started

--- rhythmboxInterface/rhythmboxInterface.py
+++ rhythmboxInterface/rhythmboxInterface.py
@@ -13,24 +13,27 @@ dbusObject = '/org/mpris/MediaPlayer2'
 tmpFilePath = '/tmp/.rhythmboxWasPlaying'
 
 #setup dbus objects
-bus = dbus.SessionBus()
-proxy = bus.get_object(dbusBusName, dbusObject)
-playerInterface = dbus.Interface(proxy, 'org.mpris.MediaPlayer2.Player')
-propertiesManager = dbus.Interface(proxy, 'org.freedesktop.DBus.Properties')
+try:
+    bus = dbus.SessionBus()
+    proxy = bus.get_object(dbusBusName, dbusObject)
+    playerInterface = dbus.Interface(proxy, 'org.mpris.MediaPlayer2.Player')
+    propertiesManager = dbus.Interface(proxy, 'org.freedesktop.DBus.Properties')
 
-#get current state
-curStatus = propertiesManager.Get('org.mpris.MediaPlayer2.Player', 'PlaybackStatus')
+    #get current state
+    curStatus = propertiesManager.Get('org.mpris.MediaPlayer2.Player', 'PlaybackStatus')
 
-tmpFile = pathlib.Path(tmpFilePath)
+    tmpFile = pathlib.Path(tmpFilePath)
 
-if tmpFile.is_file(): #rhythmbox was playing before
-    if not curStatus == "Playing":
-        playerInterface.PlayPause()
-    os.remove(tmpFilePath)
+    if tmpFile.is_file(): #rhythmbox was playing before
+        if not curStatus == "Playing":
+            playerInterface.PlayPause()
+        os.remove(tmpFilePath)
 
-else:
-    if curStatus == "Playing":
-        with open(tmpFilePath,'a'):
-            os.utime(tmpFilePath, None)
-        playerInterface.PlayPause()
-    #else: #nothing do to
+    else:
+        if curStatus == "Playing":
+            with open(tmpFilePath,'a'):
+                os.utime(tmpFilePath, None)
+            playerInterface.PlayPause()
+        #else: #nothing do to
+except:
+    print("Caught exception - probably Rhythmbox is not running.")
