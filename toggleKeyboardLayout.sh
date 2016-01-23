1e904b028b571b72985a633796e7df1eca929f48
Oliver Wangler <oliver@wngr.de>
2016-01-23 18:34:21 +0100

    added small script to toggle between us and de keylayout

---
+++ toggleKeyboardLayout.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+curLayout=$(setxkbmap -query | awk '/layout/{print $2}')
+firstLayout="de"
+secondLayout="us"
+
+if [ $curLayout == $firstLayout ]
+then
+    newLayout=$secondLayout
+else
+    newLayout=$firstLayout
+fi
+echo "set layout to $newLayout"
+setxkbmap $newLayout
