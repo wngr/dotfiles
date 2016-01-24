2e025b3af879f318f77a3a4f14e50f3b57e5288c
Oliver Wangler <oliver@wngr.de>
2016-01-24 11:44:15 +0100

    added external ip display to i3status

--- i3status/i3statusAdditional.sh
+++ i3status/i3statusAdditional.sh
@@ -6,11 +6,13 @@ i3status --config ~/.config/i3status/config | while :
 do
     read line
     LG=$(setxkbmap -query | awk '/layout/{print $2}')
+    externalIp=$(dig +short myip.opendns.com @resolver1.opendns.com)
         if [ $LG == "de" ]
         then
             dat="[{ \"full_text\": \"layout: $LG\", \"color\":\"#009E00\" },"
         else
             dat="[{ \"full_text\": \"layout: $LG\", \"color\":\"#C60101\" },"
         fi
+    dat+="{ \"full_text\": \"ext. ip: $externalIp\"},"
     echo "${line/[/$dat}" || exit 1
 done
