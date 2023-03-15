c4f55a6b0a89f638f97408436dc080d16cc17479
Oliver Wangler <oliver.wangler@helsing.ai>
2023-03-15 09:39:37 +0100

    update nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -75,57 +75,63 @@ require("mason-lspconfig").setup_handlers {
     end,
     -- Next, you can provide a dedicated handler for specific servers.
     -- For example, a handler override for the `rust_analyzer`:
---    ["rust_analyzer"] = function ()
---        require("rust-tools").setup {}
---    end
+   ["rust_analyzer"] = function ()
+      local opts = {
+        tools = { -- rust-tools options
+          autoSetHints = true,
+          hover_with_actions = true,
+          inlay_hints = {
+            auto = true,
+            show_parameter_hints = true,
+            parameter_hints_prefix = "<- ",
+            other_hints_prefix = "=> ",
+            },
+        },
+
+      -- all the opts to send to nvim-lspconfig
+      -- these override the defaults set by rust-tools.nvim
+      -- see https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#rust_analyzer
+        server = {
+          -- on_attach is a callback called when the language server attachs to the buffer
+          -- on_attach = on_attach,
+          settings = {
+            -- to enable rust-analyzer settings visit:
+            -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
+            ["rust-analyzer"] = {
+              diagnostics = {
+                enable = true,
+                disabled = {"unresolved-proc-macro"},
+                enableExperimental = true,
+              },
+              -- enable clippy on save
+              checkOnSave = {
+                command = "clippy",
+      --          overrideCommand = { "cargo", "clippy", "--message-format=json" },
+      --          invocationStrategy = "once",
+      --          invocationLocation = "root",
+              },
+              procMacro = {
+                enable = true,
+              },
+              cargo = {
+      --          target = "wasm32-unknown-unknown",
+                buildScripts = {
+                  enable = true,
+      --            overrideCommand = { "cargo", "check", "--quiet", "--message-format=json" },
+      --            invocationStrategy = "once",
+      --            invocationLocation = "root",
+                }
+              },
+            }
+          }
+        },
+      }
+
+      require('rust-tools').setup(opts)
+   end
 }
 
 local nvim_lsp = require'lspconfig'
-
-local opts = {
-  tools = { -- rust-tools options
-  autoSetHints = true,
-  hover_with_actions = true,
-  inlay_hints = {
-    show_parameter_hints = false,
-    parameter_hints_prefix = "",
-    other_hints_prefix = "",
-    },
-  },
-
--- all the opts to send to nvim-lspconfig
--- these override the defaults set by rust-tools.nvim
--- see https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#rust_analyzer
-  server = {
-    -- on_attach is a callback called when the language server attachs to the buffer
-    -- on_attach = on_attach,
-    settings = {
-      -- to enable rust-analyzer settings visit:
-      -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
-      ["rust-analyzer"] = {
-        -- enable clippy on save
-        checkOnSave = {
-          command = "clippy",
---          overrideCommand = { "cargo", "clippy", "--message-format=json" },
---          invocationStrategy = "once",
---          invocationLocation = "root",
-        },
-        cargo = {
---          target = "wasm32-unknown-unknown",
---          buildScripts = {
---            enable = true,
---            overrideCommand = { "cargo", "check", "--quiet", "--message-format=json" },
---            invocationStrategy = "once",
---            invocationLocation = "root",
---          }
-        },
-      }
-    }
-  },
-}
-
-require('rust-tools').setup(opts)
-
 
 --local ts = require 'nvim-treesitter.configs'
 --ts.setup {ensure_installed = 'maintained', highlight = {enable = true}}
